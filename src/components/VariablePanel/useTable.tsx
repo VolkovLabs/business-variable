1486546b782e7fde42f8343fae1e5e8716b0ca7f
Mikhail <mikhail@volkovlabs.io>
2023-06-25 11:23:35 -0400

    Update checkbox for the last row only.

--- src/components/VariablePanel/useTable.tsx
+++ src/components/VariablePanel/useTable.tsx
@@ -199,7 +199,7 @@ export const useTable = ({
         cell: ({ row, getValue }) => {
           return (
             <div style={{ paddingLeft: `${row.depth}rem`, display: 'flex' }}>
-              {(row.original.showStatus || row.original.selected) && (
+              {!row.originalSubRows && (
                 <input
                   type={runtimeVariable?.multi ? 'checkbox' : 'radio'}
                   onChange={() => onChange(row.original)}
