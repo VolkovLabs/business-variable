e6ecb28bdc0dd61a00b5fd1daf2b11977f15a1de
Alex Simonok <sashasimonok@gmail.com>
2023-06-26 12:09:12 +0300

    Add tests for module

--- src/module.test.ts
+++ src/module.test.ts
@@ -1,5 +1,11 @@
-import { PanelPlugin } from '@grafana/data';
+import { PanelPlugin, Field, FieldType } from '@grafana/data';
+import { PanelOptions } from './types';
 import { plugin } from './module';
+
+/**
+ * Test Field
+ */
+type TestField = Pick<Field, 'name' | 'type'>;
 
 /**
  * Mock @grafana/runtime
@@ -15,20 +21,20 @@ jest.mock('@grafana/runtime', () => ({
  Plugin
  */
 describe('plugin', () => {
+  /**
+   * Builder
+   */
+  const builder: any = {
+    addSelect: jest.fn().mockImplementation(() => builder),
+    addFieldNamePicker: jest.fn().mockImplementation(() => builder),
+    addCustomEditor: jest.fn().mockImplementation(() => builder),
+  };
+
   it('Should be instance of PanelPlugin', () => {
     expect(plugin).toBeInstanceOf(PanelPlugin);
   });
 
   it('Should add inputs', () => {
-    /**
-     * Builder
-     */
-    const builder: any = {
-      addSelect: jest.fn().mockImplementation(() => builder),
-      addFieldNamePicker: jest.fn().mockImplementation(() => builder),
-      addCustomEditor: jest.fn().mockImplementation(() => builder),
-    };
-
     /**
      * Supplier
      */
@@ -40,5 +46,77 @@ describe('plugin', () => {
     expect(builder.addSelect).toHaveBeenCalled();
     expect(builder.addFieldNamePicker).toHaveBeenCalled();
     expect(builder.addCustomEditor).toHaveBeenCalled();
+  });
+
+  describe('Input Visibility', () => {
+    beforeEach(() => {
+      builder.addFieldNamePicker.mockClear();
+      builder.addSelect.mockClear();
+    });
+
+    /**
+     * Add Input Implementation
+     * @param config
+     * @param result
+     */
+    const addInputImplementation = (config: Partial<PanelOptions>, result: string[]) => (input: any) => {
+      if (input.showIf) {
+        if (input.showIf(config)) {
+          result.push(input.path);
+        }
+      } else {
+        result.push(input.path);
+      }
+      return builder;
+    };
+
+    it('Should show variable name if no levels', () => {
+      const shownOptionsPaths: string[] = [];
+
+      builder.addSelect.mockImplementation(addInputImplementation({ levels: [] }, shownOptionsPaths));
+
+      plugin['optionsSupplier'](builder);
+
+      expect(shownOptionsPaths).toEqual(expect.arrayContaining(['variable']));
+    });
+  });
+
+  describe('Settings', () => {
+    const addFieldNameImplementation =
+      (optionPath: string, allFields: TestField[], shownFields: TestField[]) => (input: any) => {
+        if (optionPath === input.path) {
+          const fields = allFields.filter(input.settings.filter);
+          shownFields.push(...fields);
+        }
+        return builder;
+      };
+
+    it('Should return only string fields for name', () => {
+      const fields: TestField[] = [
+        { name: 'string', type: FieldType.string },
+        { name: 'number', type: FieldType.number },
+      ];
+      const shownFields: TestField[] = [];
+
+      builder.addFieldNamePicker.mockImplementation(addFieldNameImplementation('name', fields, shownFields));
+
+      plugin['optionsSupplier'](builder);
+
+      expect(shownFields).toEqual([{ name: 'string', type: FieldType.string }]);
+    });
+
+    it('Should return only number fields for status', () => {
+      const fields: TestField[] = [
+        { name: 'string', type: FieldType.string },
+        { name: 'number', type: FieldType.number },
+      ];
+      const shownFields: TestField[] = [];
+
+      builder.addFieldNamePicker.mockImplementation(addFieldNameImplementation('status', fields, shownFields));
+
+      plugin['optionsSupplier'](builder);
+
+      expect(shownFields).toEqual([{ name: 'number', type: FieldType.number }]);
+    });
   });
 });
