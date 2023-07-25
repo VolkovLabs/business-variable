97b9caa7d525bf3fd3d0a834902ec83715a4c4f9
Alex Simonok <sashasimonok@gmail.com>
2023-07-25 12:04:09 +0300

    Update Variable Panel tests with getJestSelectors

--- src/components/VariablePanel/VariablePanel.test.tsx
+++ src/components/VariablePanel/VariablePanel.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { getJestSelectors } from '@volkovlabs/jest-selectors';
 import { DisplayMode } from '../../types';
 import { VariablePanel } from './VariablePanel';
 
@@ -12,8 +13,8 @@ type Props = React.ComponentProps<typeof VariablePanel>;
  * In Test Ids
  */
 const InTestIds = {
-  tableView: 'table-view',
-  minimizeView: 'minimize-view',
+  tableView: 'data-testid table-view',
+  minimizeView: 'data-testid minimize-view',
 };
 
 /**
@@ -41,18 +42,24 @@ describe('Panel', () => {
     return <VariablePanel options={options} {...(restProps as any)} />;
   };
 
+  /**
+   * Selectors
+   */
+  const getSelectors = getJestSelectors(InTestIds);
+  const selectors = getSelectors(screen);
+
   it('Should render table view', async () => {
     render(getComponent({ options: { displayMode: DisplayMode.TABLE } as any }));
-    expect(screen.getByTestId(InTestIds.tableView)).toBeInTheDocument();
+    expect(selectors.tableView()).toBeInTheDocument();
   });
 
   it('Should render minimize view', async () => {
     render(getComponent({ options: { displayMode: DisplayMode.MINIMIZE } as any }));
-    expect(screen.getByTestId(InTestIds.minimizeView)).toBeInTheDocument();
+    expect(selectors.minimizeView()).toBeInTheDocument();
   });
 
   it('Should render table view by default', async () => {
     render(getComponent({ options: { displayMode: undefined } as any }));
-    expect(screen.getByTestId(InTestIds.tableView)).toBeInTheDocument();
+    expect(selectors.tableView()).toBeInTheDocument();
   });
 });
