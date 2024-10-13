b707c5a0243c6add6dff5d359a1e1956df6eb782
vitPinchuk <vit.pinchuck@gmail.com>
2024-10-13 23:53:44 +0300

    e2e

--- test/panel.spec.ts
+++ test/panel.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@grafana/plugin-e2e';
 import { TEST_IDS } from '../src/constants/tests';
-
+import { PanelHelper, UrlHelper } from './utils';
 test.describe('Volkovlabs Variable Panel', () => {
   test('Check grafana version', async ({ grafanaVersion }) => {
     console.log('Grafana version: ', grafanaVersion);
     expect(grafanaVersion).toEqual(grafanaVersion);
   });
 
-  test('Should display Alert Info', async ({ gotoDashboardPage, dashboardPage, page }) => {
+  test('Should add new empty variable panel', async ({ gotoDashboardPage, readProvisionedDashboard, page }) => {
     /**
      * Go To Panels dashboard panels.json
      * return dashboardPage
      */
-    await gotoDashboardPage({ uid: 'O4tc_E6Gz' });
+    const dashboard = await readProvisionedDashboard({ fileName: 'panels.json' });
+    const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
 
     /**
      * Await content load
@@ -20,26 +21,34 @@ test.describe('Volkovlabs Variable Panel', () => {
     await page.waitForTimeout(1500);
 
     /**
-     * Find panel by title with Alert message
-     * Should be visible
+     * Add new visualization
      */
-    await expect(dashboardPage.getPanelByTitle('Empty').locator).toBeVisible();
+    const editPage = await dashboardPage.addPanel();
+    await editPage.setVisualization('Business Variable');
+    await editPage.setPanelTitle('New Panel');
 
-    await expect(dashboardPage.getPanelByTitle('Empty').locator.getByTestId(TEST_IDS.tableView.root)).toBeVisible();
-    await expect(
-      dashboardPage
-        .getPanelByTitle('Empty')
-        .locator.getByTestId(TEST_IDS.tableView.root)
-        .getByTestId('data-testid Alert info')
-    ).toBeVisible();
+    /**
+     * Apply changes and return to dashboard
+     */
+    await editPage.backToDashboard();
+
+    /**
+     * Check Presence
+     */
+    const panel = new PanelHelper(dashboardPage, 'New Panel');
+    const tablePanel = panel.getTablePanel();
+    await tablePanel.checkPresence();
+    await tablePanel.checkAlert();
+    await panel.checkIfNoErrors();
   });
 
-  test('Should display Device Variable content', async ({ gotoDashboardPage, dashboardPage, page }) => {
+  test('Should display Device Variable content', async ({ gotoDashboardPage, readProvisionedDashboard, page }) => {
     /**
      * Go To Panels dashboard panels.json
      * return dashboardPage
      */
-    await gotoDashboardPage({ uid: 'O4tc_E6Gz' });
+    const dashboard = await readProvisionedDashboard({ fileName: 'panels.json' });
+    const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
 
     /**
      * Await content load
@@ -47,17 +56,291 @@ test.describe('Volkovlabs Variable Panel', () => {
     await page.waitForTimeout(1500);
 
     /**
-     * Find panel by title Device
-     * Should be visible
+     * Check Presence
      */
-    await expect(dashboardPage.getPanelById('6').locator.getByTestId(TEST_IDS.tableView.root)).toBeVisible();
+    const panel = new PanelHelper(dashboardPage, 'Device Simple');
+    const tablePanel = panel.getTablePanel();
+    await tablePanel.checkPresence();
 
-    /**
-     * Compare Screenshot
-     */
-    await expect(dashboardPage.getPanelById('6').locator.getByTestId(TEST_IDS.tableView.root)).toHaveScreenshot(
-      'actual-screenshot.png',
-      { maxDiffPixelRatio: 0.1 }
-    );
+    const table = tablePanel.getTable();
+    await table.getRow(0).getCell('All', 0).checkPresence();
+    await table.getRow(1).getCell('Device1', 0).checkText('Device1');
+    await table.getRow(2).getCell('Device2', 0).checkText('Device2');
+    await table.getRow(3).getCell('Device3', 0).checkText('Device3');
+    await table.getRow(4).getCell('Device4', 0).checkText('Device4');
+    await table.getRow(5).getCell('Device5', 0).checkText('Device5');
+    await table.getRow(6).getCell('Device6', 0).checkText('Device6');
+    await table.getRow(7).getCell('Device7', 0).checkText('Device7');
+  });
+
+  test.describe('Minimize view', () => {
+    test('Should change variable value using minimize view', async ({
+      gotoDashboardPage,
+      readProvisionedDashboard,
+      page,
+    }) => {
+      /**
+       * Go To Panels dashboard home.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'home.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      const panel = new PanelHelper(dashboardPage, 'Minimize');
+      const minimizePanel = panel.getMinimizePanel();
+      await minimizePanel.checkPresence();
+      /**
+       * Change variable value
+       */
+      minimizePanel.getTextInput().changeInputValue('test');
+      await page.waitForTimeout(200);
+      const urlParams = new UrlHelper(await page.url());
+      urlParams.checkVariable('var-text', 'test');
+    });
+    test('Should change variable value using minimize view and select', async ({
+      gotoDashboardPage,
+      readProvisionedDashboard,
+      page,
+    }) => {
+      /**
+       * Go To Panels dashboard home.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'panels.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      const panel = new PanelHelper(dashboardPage, 'Minimize');
+      const minimizePanel = panel.getMinimizePanel();
+      await minimizePanel.checkPresence();
+      /**
+       * Change variable value
+       */
+      await minimizePanel.getSelect().changeValue(dashboardPage, 'myKey');
+      await page.waitForTimeout(200);
+      const urlParams = new UrlHelper(await page.url());
+      urlParams.checkVariable('var-test', 'myvalue');
+    });
+  });
+
+  test.describe('Slider View', () => {
+    test('Should display Alert Info if variable not specified in Slider view', async ({
+      gotoDashboardPage,
+      readProvisionedDashboard,
+      page,
+    }) => {
+      /**
+       * Go To Panels dashboard panels.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'sliders.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      const panel = new PanelHelper(dashboardPage, 'Empty');
+      const sliderPanel = panel.getSliderPanel();
+      await sliderPanel.checkVariableAlert();
+    });
+  });
+
+  test.describe('Button view', () => {
+    test('Should change variable value using button view', async ({
+      gotoDashboardPage,
+      readProvisionedDashboard,
+      page,
+    }) => {
+      /**
+       * Go To Panels dashboard panels.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'panels.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      /**
+       * Check Presence
+       */
+      const panel = new PanelHelper(dashboardPage, 'Device 1 Multi + All');
+      const buttonPanel = panel.getButtonPanel();
+      await buttonPanel.checkPresence();
+      const button = await buttonPanel.getButton('Device4');
+      await expect(button).toBeVisible();
+      await buttonPanel.clickOnButton('Device4');
+      await page.waitForTimeout(300);
+      /**
+       * Check variable values
+       */
+      const urlParams = new UrlHelper(await page.url());
+      urlParams.checkVariable('var-device', 'Device4');
+    });
+
+    test('Should Reset variable values to initial state', async ({
+      gotoDashboardPage,
+      readProvisionedDashboard,
+      page,
+    }) => {
+      /**
+       * Go To Panels dashboard panels.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'panels.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      /**
+       * Check Presence
+       */
+      const panel = new PanelHelper(dashboardPage, 'Device 1 Multi + All');
+      const buttonPanel = panel.getButtonPanel();
+      await buttonPanel.checkPresence();
+      const button = await buttonPanel.getButton('Device3');
+      await expect(button).toBeVisible();
+      await buttonPanel.clickOnButton('Device3');
+      await page.waitForTimeout(200);
+      /**
+       * Check variable values
+       */
+      const urlParams = new UrlHelper(await page.url());
+      urlParams.checkVariable('var-device', 'Device3');
+      await buttonPanel.clickOnResetButton();
+      await page.waitForTimeout(200);
+      urlParams.updateParams(await page.url());
+      urlParams.checkIfNoVariableValue('var-device', 'Device3');
+    });
+  });
+
+  test.describe('Table view', () => {
+    test('Should display Alert Info if variable not specified in Table View', async ({
+      gotoDashboardPage,
+      readProvisionedDashboard,
+      page,
+    }) => {
+      /**
+       * Go To Panels dashboard panels.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'panels.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      const panel = new PanelHelper(dashboardPage, 'Empty');
+      const tablePanel = panel.getTablePanel();
+      await tablePanel.checkPresence();
+      await tablePanel.checkAlert();
+    });
+
+    test('Should display All option if variable include-all', async ({
+      gotoDashboardPage,
+      readProvisionedDashboard,
+      page,
+    }) => {
+      /**
+       * Go To Panels dashboard home.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'home.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      const panel = new PanelHelper(dashboardPage, 'City variable');
+      const tablePanel = panel.getTablePanel();
+      await tablePanel.checkPresence();
+      await tablePanel.getTable().getRow(0).getCell('All', 0).checkPresence();
+      await tablePanel.getTable().checkBodyRowsCount(6);
+    });
+
+    test('Should display tree cells in depth', async ({ gotoDashboardPage, readProvisionedDashboard, page }) => {
+      /**
+       * Go To Panels dashboard home.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'home.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      const panel = new PanelHelper(dashboardPage, 'Metrics');
+      const tablePanel = panel.getTablePanel();
+      await tablePanel.checkPresence();
+      // await tablePanel.getTable().getRow(0).getCell('All', 0).checkPresence();
+
+      /**
+       * Check row and cells in 4 level
+       */
+      await tablePanel.getTable().checkBodyRowsCount(16);
+      await tablePanel.getTable().getRow('0.0.0.0').checkPresence();
+      await tablePanel.getTable().getRow('0.0.0.0').getCell('NY Central 133', 3).checkPresence();
+      await tablePanel.getTable().getRow('0.0.0.0').getCell('NY Central 133', 3).checkText('NY Central 133');
+    });
+
+    test('Should expand all rows', async ({ gotoDashboardPage, readProvisionedDashboard, page }) => {
+      /**
+       * Go To Panels dashboard home.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'home.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      const panel = new PanelHelper(dashboardPage, 'Metrics');
+      const tablePanel = panel.getTablePanel();
+      await tablePanel.checkPresence();
+
+      await tablePanel.getTable().checkBodyRowsCount(16);
+      await panel.getTablePanel().getTableHeader().expandAll();
+      await tablePanel.getTable().checkBodyRowsCount(5);
+      await panel.getTablePanel().getTableHeader().expandAll();
+      await tablePanel.getTable().checkBodyRowsCount(16);
+    });
+
+    test('Should set variable using tree view', async ({ gotoDashboardPage, readProvisionedDashboard, page }) => {
+      /**
+       * Go To Panels dashboard home.json
+       * return dashboardPage
+       */
+      const dashboard = await readProvisionedDashboard({ fileName: 'home.json' });
+      const dashboardPage = await gotoDashboardPage({ uid: dashboard.uid });
+      /**
+       * Await content load
+       */
+      await page.waitForTimeout(1500);
+      const panel = new PanelHelper(dashboardPage, 'City variable');
+      const tablePanel = panel.getTablePanel();
+      await tablePanel.checkPresence();
+
+      await tablePanel.getTable().getRow(1).checkPresence();
+      await tablePanel.getTable().getRow(1).checkControl();
+      await tablePanel.getTable().getRow(1).toggleValue();
+
+      await page.waitForTimeout(200);
+
+      const urlParams = new UrlHelper(await page.url());
+      urlParams.checkVariable('var-city', 'Moscow');
+
+      await tablePanel.getTable().getRow(0).checkPresence();
+      await tablePanel.getTable().getRow(0).checkControl();
+      await tablePanel.getTable().getRow(0).toggleValue();
+      await page.waitForTimeout(200);
+      urlParams.updateParams(await page.url());
+      urlParams.checkIfNoVariableValue('var-city', 'Moscow');
+    });
   });
 });
---
+++ test/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './panels';
+export * from './selectors';
+export * from './url';
