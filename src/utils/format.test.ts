ee4074030d82ce692fbf7788b717837dc9f68001
vitPinchuk <vit.pinchuck@gmail.com>
2025-01-14 13:34:12 +0300

    test updated

--- src/utils/format.test.ts
+++ src/utils/format.test.ts
@@ -1,18 +1,18 @@
 import { getDateInLocalTimeFormat } from './format';
 
 describe('getDateInLocalTimeFormat', () => {
-  it('formats date correctly for standard date', () => {
+  it('Formats date correctly for standard date', () => {
     const date = new Date('2025-01-09T00:00:00');
-    expect(getDateInLocalTimeFormat(date)).toBe('2025-01-09');
+    expect(getDateInLocalTimeFormat(date)).toEqual('2025-01-09');
   });
 
   it('Formats date correctly for end of month', () => {
     const date = new Date('2025-01-31T00:00:00');
-    expect(getDateInLocalTimeFormat(date)).toBe('2025-01-31');
+    expect(getDateInLocalTimeFormat(date)).toEqual('2025-01-31');
   });
 
   it('Formats date correctly for February in a leap year', () => {
     const date = new Date('2024-02-29T00:00:00');
-    expect(getDateInLocalTimeFormat(date)).toBe('2024-02-29');
+    expect(getDateInLocalTimeFormat(date)).toEqual('2024-02-29');
   });
 });
